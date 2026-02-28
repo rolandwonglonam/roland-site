2c2f1baa65e064cd917f1c1744c9ee216f648146
Roland <rolandwonglonam@gmail.com>
2026-02-28 14:03:36 +1000

    fix: 修复排版问题，添加fallback样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-import typography from '@tailwindcss/typography';
-
 import cloudflare from '@astrojs/cloudflare';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss({
-      plugins: [typography()]
-    })]
+    plugins: [tailwindcss()]
   },
 
   adapter: cloudflare(),
